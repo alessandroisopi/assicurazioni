464383cbad0833abd21b1e139ce4743a03ec7a41
david <davidecalcagni.dc@gmail.com>
2025-09-03 17:55:21 +0200

    creato metodo post per l'inserimento

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/Controller.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/Controller.java
@@ -1,14 +1,15 @@
 package it.onyx.assicurazioni.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping(path = "/polizza")
 public class Controller {
 
-    @GetMapping
+    @PostMapping(path = "/insert", produces = "application/json", consumes = "application/json")
     public String index() {
         return "Hello World";
     }
