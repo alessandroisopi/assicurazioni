f0ef1b95cc5375a596f43865b3afea73f7c22d66
david <davidecalcagni.dc@gmail.com>
2025-09-03 17:39:59 +0200

    creazione controller

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/Controller.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/Controller.java
@@ -1,4 +1,15 @@
 package it.onyx.assicurazioni.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
 public class Controller {
+
+    @GetMapping
+    public String index() {
+        return "Hello World";
+    }
 }
