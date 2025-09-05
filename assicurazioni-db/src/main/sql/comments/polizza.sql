30fcfba0413bc572dc14677559ddb39ca9ad89b8
david <davidecalcagni.dc@gmail.com>
2025-09-05 11:52:52 +0200

    aggiunti lo script per i commenti

---
+++ assicurazioni-db/src/main/sql/comments/polizza.sql
@@ -0,0 +1,9 @@
+COMMENT ON COLUMN POLIZZA.ID_POLIZZA IS 'identificativo polizza';
+COMMENT ON COLUMN POLIZZA.ID_TIPO_POLIZZA IS 'identificativo tipo polizza';
+COMMENT ON COLUMN POLIZZA.ID_CLASSE IS 'identificativo classe polizza';
+COMMENT ON COLUMN POLIZZA.ID_INTESTATARIO IS 'identificativo intestatario polizza';
+COMMENT ON COLUMN POLIZZA.ID_STATO_POLIZZA IS 'identificativo stato polizza';
+COMMENT ON COLUMN POLIZZA.NUM_POLIZZA IS 'numero della polizza';
+COMMENT ON COLUMN POLIZZA.DT_INIZIO IS 'data inizio polizza';
+COMMENT ON COLUMN POLIZZA.DT_FINE IS 'data fine polizza';
+COMMENT ON COLUMN POLIZZA.NOTE IS 'note polizza';
