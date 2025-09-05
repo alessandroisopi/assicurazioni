7840382a11032cd868e69bb850b509b4ce7d12b2
david <davidecalcagni.dc@gmail.com>
2025-09-05 10:49:46 +0200

    cancellato script creato per errore

--- assicurazioni-springboot/src/main/sql/createtable/CreatePolizza.sql
+++
@@ -1,9 +0,0 @@
-create table POLIZZA (
-     ID_POLIZZA NUMBER NOT NULL PRIMARY KEY, --identificativo polizza
-     ID_TIPO_POLIZZA NUMBER NOT NULL, --identificativo tipo polizza
-     ID_CLASSE NUMBER NOT NULL, --identificativo classe polizza
-     ID_INTESTATARIO NUMBER NOT NULL, --identificativo intestatario polizza
-     ID_STATO_POLIZZA NUMBER NOT NULL, --identificativo stato polizza
-     DT_INIZIO DATE NOT NULL, --data inizio polizza
-     DT_FINE DATE NOT NULL --data fine polizza
-);
