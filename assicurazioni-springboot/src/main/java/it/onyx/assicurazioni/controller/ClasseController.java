d57e0f5a8e3a929d65fe9d27e74e745deb04a6de
david <davidecalcagni.dc@gmail.com>
2025-09-10 14:21:25 +0200

    aggiunti commenti alle controller

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/ClasseController.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/ClasseController.java
@@ -20,31 +20,31 @@ public class ClasseController {
 
     @GetMapping(path = "/getAll", produces = "application/json")
     public ResponseEntity<List<ClasseDTO>> getAll() {
-        List<ClasseDTO> result = classeService.getAll();
+        List<ClasseDTO> result = classeService.getAll();    //ottiene la lista di risultati
         if (!result.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.OK).body(result);
+            return ResponseEntity.status(HttpStatus.OK).body(result);   //se c'è almeno un risultato torna lo stato ok con la lista
         } else  {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); //se non c'è nulla torna lo stato not found
         }
     }
 
     @GetMapping(path = "/getById/{id}", produces = "application/json")
     public ResponseEntity<ClasseDTO> getById(@PathVariable("id") long id) {
-        ClasseDTO result = classeService.getById(id);
+        ClasseDTO result = classeService.getById(id);   //ottiene il risultato
         if (result != null) {
-            return ResponseEntity.status(HttpStatus.OK).body(result);
+            return ResponseEntity.status(HttpStatus.OK).body(result);   //se c'è un riscontro torna lo stato ok e l'oggetto
         }  else  {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); //se non c'è nulla torna lo stato not found
         }
     }
 
     @PutMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<ClasseDTO> update(@Validated(OnUpdate.class) @RequestBody ClasseDTO dto) {
-        ClasseDTO result = classeService.update(dto);
+        ClasseDTO result = classeService.update(dto);   //ottiene il risultato
         if (result != null) {
-            return ResponseEntity.status(HttpStatus.OK).body(result);
+            return ResponseEntity.status(HttpStatus.OK).body(result);   //se update va a buon fine torna lo stato ok e l'oggetto
         } else   {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); //se non trova l'oggetto torna lo stato not found
         }
     }
 }
--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/PolizzaController.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/PolizzaController.java
@@ -23,51 +23,51 @@ public class PolizzaController {
 
     @PostMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<PolizzaDTO> insert(@Validated(OnCreate.class) @RequestBody PolizzaDTO dto) {
-        PolizzaDTO polizzaDTO = polizzaService.insert(dto);
+        PolizzaDTO polizzaDTO = polizzaService.insert(dto); //avvia l'insert e salva il risultato
         if (polizzaDTO != null) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(polizzaDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(polizzaDTO);  //se l'insert va a buon fine torna lo stato ok e l'oggetto completo
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();   //se qualcosa non va torna lo stato bad request
         }
     }
 
     @GetMapping(path = "/getAll", produces = "application/json")
     public ResponseEntity<List<PolizzaDTO>> getAll() {
-        List<PolizzaDTO> polizzaDTO = polizzaService.getAll();
+        List<PolizzaDTO> polizzaDTO = polizzaService.getAll();  //ottiene la lista con i risltati
         if (!polizzaDTO.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.OK).body(polizzaDTO);
+            return ResponseEntity.status(HttpStatus.OK).body(polizzaDTO);   //se c'è almeno un risultato torna lo stato ok e la lista
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); //se non trova nulla torna lo stato not found
         }
     }
 
     @GetMapping(path = "/getById/{id}/{dtInserimento}", produces = "application/json")
     public ResponseEntity<PolizzaDTO> getById(@PathVariable("id") long id, @PathVariable("dtInserimento") @DateTimeFormat LocalDate  dtInserimento) {
-        PolizzaDTO polizzaDTO = polizzaService.getById(id, dtInserimento);
+        PolizzaDTO polizzaDTO = polizzaService.getById(id, dtInserimento);  //ottiene il risultato
         if (polizzaDTO != null) {
-            return ResponseEntity.status(HttpStatus.OK).body(polizzaDTO);
+            return ResponseEntity.status(HttpStatus.OK).body(polizzaDTO);   //se c'è un riscontro torna lo stato ok e l'oggetto
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); //se non è nulla torna lo stato not found
         }
     }
 
     @PutMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<PolizzaDTO> update(@Validated(OnUpdate.class) @RequestBody PolizzaDTO dto) {
-        PolizzaDTO polizzaDTO = polizzaService.update(dto);
+        PolizzaDTO polizzaDTO = polizzaService.update(dto); //esegue update
         if (polizzaDTO != null) {
-            return ResponseEntity.status(HttpStatus.OK).body(polizzaDTO);
+            return ResponseEntity.status(HttpStatus.OK).body(polizzaDTO);   //se c'è un risultato torna lo stato ok e il nuovo oggetto modificato
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); //se non trova l'oggetto o ci sono stati problemi torna lo stato not found
         }
     }
 
     @DeleteMapping(path = "/{id}/{dtInserimento}", produces = "application/json")
     public ResponseEntity<Void> delete(@PathVariable("id") long id,@PathVariable("dtInserimento") @DateTimeFormat LocalDate dtInserimento) {
-        boolean result = polizzaService.delete(id, dtInserimento);
+        boolean result = polizzaService.delete(id, dtInserimento);  //esegue la delete
         if (result) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();    //se torna true la delete è andata bene e torna lo stato no content
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();     //se non trova l'oggetto da eliminare torna lo stato not found
         }
     }
 }
