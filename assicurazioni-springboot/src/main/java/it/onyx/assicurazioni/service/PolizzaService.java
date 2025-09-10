249efcce8713391b3ae9a0bac41d2f049d1f98d1
david <davidecalcagni.dc@gmail.com>
2025-09-10 12:04:51 +0200

    sistemati dei warning

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/service/PolizzaService.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/service/PolizzaService.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 public interface PolizzaService {
     //metodi crud polizza
-    public PolizzaDTO insert(PolizzaDTO p);    //inserimento
-    public List<PolizzaDTO> getAll();  //prendi tutti
-    public PolizzaDTO getById(long idPolizza, LocalDate dtInserimento); //prendi per id
-    public PolizzaDTO update(PolizzaDTO p);    //aggiorna passando l'oggetto
-    public boolean delete(long idPolizza, LocalDate dtInserimento);  //cancella la polizza
+    PolizzaDTO insert(PolizzaDTO p);    //inserimento
+    List<PolizzaDTO> getAll();  //prendi tutti
+    PolizzaDTO getById(long idPolizza, LocalDate dtInserimento); //prendi per id
+    PolizzaDTO update(PolizzaDTO p);    //aggiorna passando l'oggetto
+    boolean delete(long idPolizza, LocalDate dtInserimento);  //cancella la polizza
 }
