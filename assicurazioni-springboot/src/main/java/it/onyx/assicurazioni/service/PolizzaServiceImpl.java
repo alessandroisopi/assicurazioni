1e8f42165219f04c9a714e2bb3c1509012df0de1
david <davidecalcagni.dc@gmail.com>
2025-09-10 11:07:08 +0200

    sistemata l'insert per il nuovo id e anche per il campo Classe, cambiato modo di ottenere l'id di polizza nei metodi infine aggiunti e modificati alcuni commenti

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/service/PolizzaServiceImpl.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/service/PolizzaServiceImpl.java
@@ -1,8 +1,11 @@
 package it.onyx.assicurazioni.service;
 
 import it.onyx.assicurazioni.dto.PolizzaDTO;
+import it.onyx.assicurazioni.entity.Classe;
 import it.onyx.assicurazioni.entity.Polizza;
+import it.onyx.assicurazioni.entity.PolizzaEmbeddedId;
 import it.onyx.assicurazioni.entity.TipoPolizza;
+import it.onyx.assicurazioni.repository.ClasseRepository;
 import it.onyx.assicurazioni.repository.PolizzaRepository;
 import it.onyx.assicurazioni.repository.TipoPolizzaRepository;
 import it.onyx.assicurazioni.util.ClasseMapper;
@@ -12,6 +15,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,18 +28,23 @@ public class PolizzaServiceImpl implements PolizzaService {
     @Autowired
     private TipoPolizzaRepository tipoPolizzaRepository;
 
+    @Autowired
+    private ClasseRepository classeRepository;
+
     @Override
     public PolizzaDTO insert(PolizzaDTO dto) {
         try {
             dto.setCombinato();
-            Polizza polizza = PolizzaMapper.daPolizzaDTOAPolizza(dto);    //conversione ad entità del dto
-            TipoPolizza tipoPolizza = tipoPolizzaRepository.findById(dto.getIdTipoPolizza().getIdTipoPolizza()).get();
-            polizza.setIdTipoPolizza(tipoPolizza);
+            Polizza polizza = PolizzaMapper.daPolizzaDTOAPolizza(dto);    //conversione a entità del dto
+            TipoPolizza tipoPolizza = tipoPolizzaRepository.findById(dto.getIdTipoPolizza().getIdTipoPolizza()).get();  //ottiene il tipo di polizza tramite l'id
+            polizza.setIdTipoPolizza(tipoPolizza);  //viene settato il tipo di polizza
+            Classe classe = classeRepository.findById(dto.getIdClasse().getIdClasse()).get();   //ottiene la classe della polizza tramite l'id
+            polizza.setIdClasse(classe);    //viene settata la classe
             if(dto.getDtFine().isBefore(dto.getDtInizio())) {   //controllo che la data sia coerente
                 return null;
             }
-            polizza = polizzaRepository.save(polizza);  //effetua il salvataggio sia nel database che nella variabile
-            if (polizzaRepository.existsById(polizza.getIdPolizza())) { //controllo se andato tutto bene
+            polizza = polizzaRepository.save(polizza);  //effettua il salvataggio sia nel database che nella variabile
+            if (polizzaRepository.existsById(polizza.getId())) { //controllo se andato tutto bene
                 return PolizzaMapper.daPolizzaAPolizzaDTO(polizza);
             } else {
                 return null;
@@ -49,7 +58,7 @@ public class PolizzaServiceImpl implements PolizzaService {
     @Override
     public List<PolizzaDTO> getAll() {
         try {
-            List<PolizzaDTO> result = new ArrayList<>();    //istanziata lista per il risultato
+            List<PolizzaDTO> result = new ArrayList<>();    //crea la lista per il risultato
             for (Polizza p : polizzaRepository.findAll()) { //itera tutta la tabella e inserisce ogni riga nella lista
                 result.add(PolizzaMapper.daPolizzaAPolizzaDTO(p));
             }
@@ -61,8 +70,9 @@ public class PolizzaServiceImpl implements PolizzaService {
     }
 
     @Override
-    public PolizzaDTO getById(long id) {
+    public PolizzaDTO getById(long idPolizza, LocalDate dtInserimento) {
         try {
+            PolizzaEmbeddedId id = new PolizzaEmbeddedId(idPolizza, dtInserimento); //viene creato direttamente l'oggetto della chiave composta per comodità
             if (polizzaRepository.existsById(id)) { //controlla l'esistenza e ritorna l'oggetto
                 return PolizzaMapper.daPolizzaAPolizzaDTO(polizzaRepository.findById(id).get());
             } else {
@@ -77,9 +87,10 @@ public class PolizzaServiceImpl implements PolizzaService {
     @Override
     public PolizzaDTO update(PolizzaDTO dto) {
        try {
-           if (polizzaRepository.existsById(dto.getIdPolizza())) { //controlla l'esistenza dell'oggetto
-               Polizza polizzaDB = polizzaRepository.findById(dto.getIdPolizza()).get();   //prende il campo nel db per riempire le colonne che non devono essere aggiornate
-               if (dto.getIdTipoPolizza().getIdTipoPolizza() == 0) {  //una serie di if che controlla campo per campo
+           PolizzaEmbeddedId id = new PolizzaEmbeddedId(dto.getIdPolizza(), dto.getDtInserimento());
+           if (polizzaRepository.existsById(id)) { //controlla l'esistenza dell'oggetto
+               Polizza polizzaDB = polizzaRepository.findById(id).get();   //prende il campo nel db per riempire le colonne che non devono essere aggiornate
+               if (dto.getIdTipoPolizza().getIdTipoPolizza() == 0) {  //una serie di if che controlla campo per campo, se nulli vengono rimpiazzati con quelli da database
                    dto.setIdTipoPolizza(TipoPolizzaMapper.daTipoPolizzaATipoPolizzaDTO(polizzaDB.getIdTipoPolizza()));
                }
                if (dto.getIdClasse().getIdClasse() == 0) {
@@ -115,8 +126,9 @@ public class PolizzaServiceImpl implements PolizzaService {
     }
 
     @Override
-    public boolean delete(long id) {
+    public boolean delete(long idPolizza, LocalDate dtInserimento) {
         try {
+            PolizzaEmbeddedId id = new PolizzaEmbeddedId(idPolizza, dtInserimento); //viene creato direttamente l'oggetto della chiave composta per comodità
             if (polizzaRepository.existsById(id)) { //controlla l'esistenza
                 polizzaRepository.deleteById(id);   //lo elimina
                 return true;    //torna true
