aacd156e5c5fc427783b2520e790525ae78c11e5
david <davidecalcagni.dc@gmail.com>
2025-09-10 11:03:33 +0200

    modifica di alcuni parametri dei metodi a causa del nuovo id di polizza

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/service/PolizzaService.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/service/PolizzaService.java
@@ -2,13 +2,14 @@ package it.onyx.assicurazioni.service;
 
 import it.onyx.assicurazioni.dto.PolizzaDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PolizzaService {
     //metodi crud polizza
     public PolizzaDTO insert(PolizzaDTO p);    //inserimento
     public List<PolizzaDTO> getAll();  //prendi tutti
-    public PolizzaDTO getById(long id); //prendi per id
+    public PolizzaDTO getById(long idPolizza, LocalDate dtInserimento); //prendi per id
     public PolizzaDTO update(PolizzaDTO p);    //aggiorna passando l'oggetto
-    public boolean delete(long id);  //cancella la polizza
+    public boolean delete(long idPolizza, LocalDate dtInserimento);  //cancella la polizza
 }
