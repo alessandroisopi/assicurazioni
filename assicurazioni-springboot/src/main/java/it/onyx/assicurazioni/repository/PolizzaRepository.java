ddd2021583c7ea79b101866c9fa37d6f1e8a713e
david <davidecalcagni.dc@gmail.com>
2025-09-10 10:58:47 +0200

    cambiato id di polizza con embedded id

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/repository/PolizzaRepository.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/repository/PolizzaRepository.java
@@ -1,9 +1,10 @@
 package it.onyx.assicurazioni.repository;
 
 import it.onyx.assicurazioni.entity.Polizza;
+import it.onyx.assicurazioni.entity.PolizzaEmbeddedId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PolizzaRepository extends JpaRepository<Polizza, Long> {
+public interface PolizzaRepository extends JpaRepository<Polizza, PolizzaEmbeddedId> {
 }
