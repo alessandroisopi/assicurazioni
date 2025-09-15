a179c890e2f4554865ab8e53ea98fc9c4a418cbf
david <davidecalcagni.dc@gmail.com>
2025-09-15 12:25:50 +0200

    aggiunta classe utente

---
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/entity/Utente.java
@@ -0,0 +1,53 @@
+package it.onyx.assicurazioni.entity;
+
+import java.time.LocalDate;
+
+public class Utente {
+
+    private String CF;
+    private String nome;
+    private String cognome;
+    private LocalDate dataNascita;
+
+    public Utente(String CF, String nome, String cognome, LocalDate dataNascita) {
+        this.CF = CF;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+    }
+
+    public Utente() {
+    }
+
+    public String getCF() {
+        return CF;
+    }
+
+    public void setCF(String CF) {
+        this.CF = CF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+}
