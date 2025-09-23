eafc54b1aa142c51bf9ad910515fa7079e778a31
david <davidecalcagni.dc@gmail.com>
2025-09-23 14:50:38 +0200

    creata query

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/repository/PolizzaRepository.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/repository/PolizzaRepository.java
@@ -14,4 +14,10 @@ public interface PolizzaRepository extends JpaRepository<Polizza, PolizzaEmbedde
                                         "FROM POLIZZA " +
                                         "WHERE ID_POLIZZA = :id)")
     Polizza getById(@Param("id") long id);
+
+    @Query(nativeQuery = true, value = """
+            SELECT P.* FROM POLIZZA P, STATO_POLIZZA SP, TIPO_POLIZZA TP WHERE TP.ID_TIPO_POLIZZA = P.ID_TIPO_POLIZZA AND SP.ID_STATO_POLIZZA = P.ID_STATO_POLIZZA AND\s
+            P.CD_INTESTATARIO = :id AND SP.ID_STATO_POLIZZA = 1 AND TP.ID_TIPO_POLIZZA = 2 AND P.DT_INSERIMENTO = (SELECT MAX(DT_INSERIMENTO) FROM POLIZZA WHERE CD_INTESTATARIO = :id) AND\s
+            SYSDATE BETWEEN P.DT_INIZIO AND P.DT_FINE""")
+    Polizza getByCdIntestatario(@Param("id") String id);
 }
