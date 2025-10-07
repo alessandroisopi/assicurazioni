e8206619a1971eda51551c3182b62ce86e447b98
david <davidecalcagni.dc@gmail.com>
2025-10-07 11:50:53 +0200

    creata entità con id per documentoPolizza

---
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/entity/DocumentoPolizza.java
@@ -0,0 +1,86 @@
+package it.onyx.assicurazioni.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "DOCUMENTO_POLIZZA")
+public class DocumentoPolizza {
+
+    @EmbeddedId
+    private DocumentoPolizzaEmbeddedId id;
+
+    @Column(name = "ID_POLIZZA")
+    private long idPolizza;
+
+    @Column(name = "DOC")
+    private byte[] doc;
+
+    @Column(name = "NOME_FILE")
+    private String nomeFile;
+
+    @Column(name = "UTENTE_C")
+    private String utenteC;
+
+    @Transient
+    private Polizza polizza;
+
+    public DocumentoPolizza() {
+    }
+
+    public DocumentoPolizza(DocumentoPolizzaEmbeddedId id, long idPolizza, byte[] doc, String nomeFile, String utenteC, Polizza polizza) {
+        this.id = id;
+        this.idPolizza = idPolizza;
+        this.doc = doc;
+        this.nomeFile = nomeFile;
+        this.utenteC = utenteC;
+        this.polizza = polizza;
+    }
+
+    public DocumentoPolizzaEmbeddedId getId() {
+        return id;
+    }
+
+    public void setId(DocumentoPolizzaEmbeddedId id) {
+        this.id = id;
+    }
+
+    public long getIdPolizza() {
+        return idPolizza;
+    }
+
+    public void setIdPolizza(long idPolizza) {
+        this.idPolizza = idPolizza;
+    }
+
+    public byte[] getDoc() {
+        return doc;
+    }
+
+    public void setDoc(byte[] doc) {
+        this.doc = doc;
+    }
+
+    public String getNomeFile() {
+        return nomeFile;
+    }
+
+    public void setNomeFile(String nomeFile) {
+        this.nomeFile = nomeFile;
+    }
+
+    public String getUtenteC() {
+        return utenteC;
+    }
+
+    public void setUtenteC(String utenteC) {
+        this.utenteC = utenteC;
+    }
+
+    public Polizza getPolizza() {
+        return polizza;
+    }
+
+    public void setPolizza(Polizza polizza) {
+        this.polizza = polizza;
+    }
+}
---
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/entity/DocumentoPolizzaEmbeddedId.java
@@ -0,0 +1,41 @@
+package it.onyx.assicurazioni.entity;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Embeddable
+public class DocumentoPolizzaEmbeddedId implements Serializable {
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_documento_polizza")
+    @SequenceGenerator(name = "seq_documento_polizza", sequenceName = "seq_documento_polizza", allocationSize = 1)
+    private long id;
+    private LocalDateTime dtInserimento;
+
+    public DocumentoPolizzaEmbeddedId() {
+    }
+
+    public DocumentoPolizzaEmbeddedId(long id, LocalDateTime dtInserimento) {
+        this.id = id;
+        this.dtInserimento = dtInserimento;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDtInserimento() {
+        return dtInserimento;
+    }
+
+    public void setDtInserimento(LocalDateTime dtInserimento) {
+        this.dtInserimento = dtInserimento;
+    }
+}
