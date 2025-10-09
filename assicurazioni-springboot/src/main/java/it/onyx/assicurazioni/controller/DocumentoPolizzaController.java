d810f5fa703a6834c0379ea36b6a8e531d4c68f6
david <davidecalcagni.dc@gmail.com>
2025-10-09 12:46:36 +0200

    controller che reso il documento dto dalla service lo trasforma in un pdf da mostare

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/DocumentoPolizzaController.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/controller/DocumentoPolizzaController.java
@@ -3,12 +3,12 @@ package it.onyx.assicurazioni.controller;
 import it.onyx.assicurazioni.dto.DocumentoPolizzaDTO;
 import it.onyx.assicurazioni.service.DocumentoPolizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path = "/documentoPolizza")
@@ -20,5 +20,17 @@ public class DocumentoPolizzaController {
     @PostMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<DocumentoPolizzaDTO> insert(@RequestBody DocumentoPolizzaDTO dto) throws Exception {
         return ResponseEntity.status(HttpStatus.OK).body(documentoPolizzaService.insert(dto));
+    }
+
+    @GetMapping(path = "getPdf/{id}")
+    public ResponseEntity<byte[]> getDocumentoPolizzaPDF(@PathVariable("id") long id) throws Exception {
+        DocumentoPolizzaDTO result = documentoPolizzaService.getById(id);
+        byte[] doc = result.getDoc();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        String nomeFile = result.getNomeFile();
+        headers.setContentDispositionFormData(nomeFile, nomeFile);
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+        return new ResponseEntity<>(doc, headers, HttpStatus.OK);
     }
 }
