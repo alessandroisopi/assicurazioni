9166f72e1217e37828b3b32384dc05c7bef7bba9
david <davidecalcagni.dc@gmail.com>
2025-10-09 12:45:44 +0200

    creata query che ritorna il documento più recente con un id

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/repository/DocumentoPolizzaRepository.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/repository/DocumentoPolizzaRepository.java
@@ -3,8 +3,15 @@ package it.onyx.assicurazioni.repository;
 import it.onyx.assicurazioni.entity.DocumentoPolizza;
 import it.onyx.assicurazioni.entity.DocumentoPolizzaEmbeddedId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DocumentoPolizzaRepository extends JpaRepository<DocumentoPolizza, DocumentoPolizzaEmbeddedId> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM DOCUMENTO_POLIZZA " +
+            "WHERE DT_INSERT = (SELECT MAX(DT_INSERT) FROM DOCUMENTO_POLIZZA " +
+            "WHERE ID = :id)")
+    DocumentoPolizza getById(@Param("id") long id);
 }
