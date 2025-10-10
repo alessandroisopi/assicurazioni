d370191e37622930002604076b0bf9858fdea8ad
david <davidecalcagni.dc@gmail.com>
2025-10-10 12:12:25 +0200

    sistemata la delete

--- assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/service/PolizzaService.java
+++ assicurazioni-springboot/src/main/java/it/onyx/assicurazioni/service/PolizzaService.java
@@ -12,7 +12,7 @@ public interface PolizzaService {
     List<PolizzaDTO> getAll();  //prendi tutti
     PolizzaDTO getById(long idPolizza); //prendi per id
     PolizzaDTO update(PolizzaDTO p);    //aggiorna passando l'oggetto
-    boolean delete(long idPolizza, LocalDateTime dtInserimento);  //cancella la polizza
+    boolean delete(long idPolizza);  //cancella la polizza
 
     boolean verificaStatoPolizza(String cd); //controlla se il cd intestatario passato è idoneo alla RCA e controlla se è ancora attiva
 
